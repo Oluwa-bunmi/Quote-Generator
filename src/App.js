3a76586b75380ada1afdf3c258eb13ea22cdc3e1
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-03-02 17:36:25 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -20,7 +20,14 @@ function App() {
   return (
     <div className="App h-full bg-city bg-cover bg-center flex items-center justify-center text-center">
       <div class="w-full h-full flex  justify-center items-center backdrop-brightness-50">
-        <div className="text-[whitesmoke]"><h1 className="text-3xl">{advice}</h1></div>
+        <div className="bg-[whitesmoke] w-[82%] h-[44%] md:w-[40%] md:h-[44%] flex flex-col items-center p-[2%] rounded-[25px]">
+          <h1 className="text-2xl h-[582px] flex items-center">{advice}</h1>
+          <button className="relative outline-none rounded-[50px] flex items-center justify-center cursor-pointer h-[200px] w-[210px] opacity-100 bg-[#fff] border border-primary hover:animate-[rotate_0.7s_ease-in-out_both]">
+            <span className="text-[#164ca7] text-xs font-medium tracking-[0.7px] hover:animate-[storm_0.7s_ease-in-out_both] delay-[0.06s]">
+              GIVE ME ADVICE!
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,28 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        "city" : "url('/src/assets/city.jpg')"
-      }
+      backgroundImage: {
+        city: "url('/src/assets/city.jpg')",
+      },
+      colors: {
+        primary: "rgba(22, 76, 167, 0.6)",
+      },
+      keyframes: {
+        rotate: {
+          0: { transform: "rotate(0deg) translate3d(0, 0, 0)" },
+          "25%": { transform: "rotate(3deg) translate3d(0, 0, 0)" },
+          "50%": { transform: "rotate(-3deg) translate3d(0, 0, 0)" },
+          "75%": { transform: "rotate(1deg) translate3d(0, 0, 0)" },
+          "100%": { transform: "rotate(0deg) translate3d(0, 0, 0)" },
+        },
+        storm: {
+          0: { transform: "translate3d(0, 0, 0) translateZ(0)" },
+          "25%": { transform: "translate3d(4px, 0, 0) translateZ(0)" },
+          "50%": { transform: "translate3d(-3px, 0, 0) translateZ(0)" },
+          "75%": { transform: "translate3d(2px, 0, 0) translateZ(0)" },
+          "100%": { transform: "translate3d(0, 0, 0) translateZ(0" },
+        },
+      },
     },
   },
   plugins: [],
